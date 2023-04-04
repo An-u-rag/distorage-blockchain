b4cf978d6407337c4dab4c70bfaa39ccefea4a79
An-u-rag <anuragparcha315@gmail.com>
2023-04-04 11:24:46 -0700

    removing node_modules from  git track

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -14,6 +14,8 @@ contract Storage {
 
     mapping(uint => Host) public hosts;
     mapping(address => bool) public isHost;
+    mapping(bytes32 => address) public location_address; 
+    mapping(bytes32 => bytes32) private locHash_genHash;
     uint public hostCount;
     
     constructor () {
@@ -21,17 +23,32 @@ contract Storage {
 
     event addHostEvent (
         uint indexed id,
-        address indexed myAddress
+        address indexed myAddress,
+        bytes32 indexed locHash
     );
 
-    function addHost (string memory _name, uint port, string memory ip, uint diskspace) public {
+    function addHost (string memory _name, uint port, string memory ip, bytes32 locHash, uint diskspace) public {
         require(!isHost[msg.sender]);
 
         hosts[hostCount] = Host(hostCount, msg.sender, ip, port, _name, diskspace);
         isHost[msg.sender] = true;
+        location_address[locHash] = msg.sender;
 
-        emit addHostEvent(hostCount, msg.sender);
+        emit addHostEvent(hostCount, msg.sender, locHash);
         hostCount ++;
     }
+
+    // event initiateStorage(
+        
+    // )
+
+    // // TO initiate storage of a file from client to host
+    // // lochash - hash of Host ip, port, and name
+    // // genHash - Hash of the encrypted file
+    // // salt - salt of the hashing function 
+    // function initiateStorage (bytes32 lochash, bytes32 genhash, uint salt) public {
+    //     // Now that we get the input, we have to emit an event which is used as a check for socket transmission
+        
+    // } 
 
 }
