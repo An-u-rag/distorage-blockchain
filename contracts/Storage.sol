4904aab88a974241d57d0349aef3ac0eab3ed817
An-u-rag <anuragparcha315@gmail.com>
2023-04-05 17:29:23 -0700

    Client Data Storage Initiation Done

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -10,15 +10,33 @@ contract Storage {
         uint port;
         string name;
         uint diskspace;
+        bytes32 locHash;
+        uint dataCount;
+    }
+
+    struct Data {
+        bytes32 genHash;
+        address clientAddress;
+        address hostAddress;
+        uint auditInterval;
+        uint size;
+        string salt;
     }
 
     mapping(uint => Host) public hosts;
     mapping(address => bool) public isHost;
-    mapping(bytes32 => address) public location_address; 
-    mapping(bytes32 => bytes32) private locHash_genHash;
+    mapping(address => uint) public clientDataCount;
+    mapping(address => bytes32[]) public clientDataMap;
+    mapping(bytes32 => Data) public datas;
+    mapping(bytes32 => Host) private dataToHost; // since the mapping is one-to-many between host and data.
     uint public hostCount;
+    uint public dataCount;
     
     constructor () {
+    }
+
+    function getClientData(address addr, uint index) public view returns(bytes32){
+        return clientDataMap[addr][index];
     }
 
     event addHostEvent (
@@ -30,25 +48,46 @@ contract Storage {
     function addHost (string memory _name, uint port, string memory ip, bytes32 locHash, uint diskspace) public {
         require(!isHost[msg.sender]);
 
-        hosts[hostCount] = Host(hostCount, msg.sender, ip, port, _name, diskspace);
+        hosts[hostCount] = Host(hostCount, msg.sender, ip, port, _name, diskspace, locHash, 0);
         isHost[msg.sender] = true;
-        location_address[locHash] = msg.sender;
 
         emit addHostEvent(hostCount, msg.sender, locHash);
         hostCount ++;
     }
 
-    // event initiateStorage(
-        
-    // )
+    event initiateStorageEvent(
+        uint indexed hostId,
+        address clientAddress,
+        uint remainingHostDiskspace,
+        uint indexed interval,
+        string indexed salt
+    );
 
-    // // TO initiate storage of a file from client to host
-    // // lochash - hash of Host ip, port, and name
-    // // genHash - Hash of the encrypted file
-    // // salt - salt of the hashing function 
-    // function initiateStorage (bytes32 lochash, bytes32 genhash, uint salt) public {
-    //     // Now that we get the input, we have to emit an event which is used as a check for socket transmission
-        
-    // } 
+    // TO initiate storage of a file from client to host
+    // lochash - hash of Host ip, port, and name
+    // genHash - Hash of the encrypted file
+    // salt - salt of the hashing function 
+    function initiateStorage (uint id, bytes32 genHash, uint size, uint interval, string memory salt) public {
+        // Overall datacount update
+        require(!isHost[msg.sender]);
+        require(isHost[hosts[id].myAddress]);
+        require(size <= hosts[id].diskspace);
+        // Now that we get the input, we have to emit an event which is used as a check for socket transmission
+
+        // Create a Data Instance
+        datas[genHash] = Data(genHash, msg.sender, hosts[id].myAddress, interval, size, salt);
+
+        // Link Data to Host
+        dataToHost[genHash] = hosts[id];
+
+        // Update diskspace and dataCount of host
+        hosts[id].diskspace -= size;
+        hosts[id].dataCount += 1;
+
+        dataCount ++;
+        clientDataCount[msg.sender] += 1;
+        clientDataMap[msg.sender].push(genHash);
+        emit initiateStorageEvent(id, msg.sender, hosts[id].diskspace, interval, salt);
+    } 
 
 }
